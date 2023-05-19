d0a00c26b642ed53524162b14ea81b5ee1a58a31
Maral <this.is.me.maral@gmail.com>
2023-05-19 11:25:31 +0200

    remove fashionMNIST

--- datasets.py
+++ datasets.py
@@ -5,10 +5,7 @@ import torchvision
 from matplotlib import pyplot as plt
 from torch.utils.data import (
     Dataset,
-    ConcatDataset,
-    Subset,
     DataLoader,
-    RandomSampler,
 )
 import numpy as np
 from torchvision import transforms
@@ -17,22 +14,18 @@ from torchvision import transforms
 def get_dataloaders(dataset_name, batch_size, seed):
     mean = {
         'MNIST': np.array([0.1307]),
-        'FashionMNIST': np.array([0.2859]),
         'cifar10': np.array([0.49139968, 0.48215827, 0.44653124])
     }
     std = {
         'MNIST': [0.3081],
-        'FashionMNIST': [0.2859],
         'cifar10': np.array([0.24703233, 0.24348505, 0.26158768])
     }
     dataset_classes = {
         'MNIST': torchvision.datasets.MNIST,
-        'FashionMNIST': torchvision.datasets.FashionMNIST,
         'cifar10': torchvision.datasets.CIFAR10
     }
     train_transforms = {
         'MNIST': [transforms.RandomCrop(28, padding=1, padding_mode='edge')],
-        'FashionMNIST': [transforms.RandomCrop(28, padding=1, padding_mode='edge')],
         'cifar10': [transforms.Resize((32, 32)),  # resises the image so it can be perfect for our model.
                     transforms.RandomHorizontalFlip(),  # FLips the image w.r.t horizontal axis
                     transforms.RandomRotation(10),  # Rotates the image to a specified angel
@@ -41,7 +34,7 @@ def get_dataloaders(dataset_name, batch_size, seed):
                     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),  # Set the color params
                     ]
     }
-    input_shape = {'MNIST': [1, 28, 28], 'FashionMNIST': [1, 28, 28], 'cifar10': [3, 32, 32]}
+    input_shape = {'MNIST': [1, 28, 28], 'cifar10': [3, 32, 32]}
 
     dataset_class = dataset_classes[dataset_name]
     default_transform = [transforms.ToTensor(),
@@ -94,22 +87,18 @@ def get_subset(dataset, labels):
 def get_dataloaders_forgetting(dataset_name, batch_size, seed):
     mean = {
         'MNIST': np.array([0.1307]),
-        'FashionMNIST': np.array([0.2859]),
         'cifar10': np.array([0.49139968, 0.48215827, 0.44653124])
     }
     std = {
         'MNIST': [0.3081],
-        'FashionMNIST': [0.2859],
         'cifar10': np.array([0.24703233, 0.24348505, 0.26158768])
     }
     dataset_classes = {
         'MNIST': torchvision.datasets.MNIST,
-        'FashionMNIST': torchvision.datasets.FashionMNIST,
         'cifar10': torchvision.datasets.CIFAR10
     }
     train_transforms = {
         'MNIST': [transforms.RandomCrop(28, padding=1, padding_mode='edge')],
-        'FashionMNIST': [transforms.RandomCrop(28, padding=1, padding_mode='edge')],
         'cifar10': [transforms.Resize((32, 32)),  # resises the image so it can be perfect for our model.
                     transforms.RandomHorizontalFlip(),  # FLips the image w.r.t horizontal axis
                     transforms.RandomRotation(10),  # Rotates the image to a specified angel
@@ -118,7 +107,7 @@ def get_dataloaders_forgetting(dataset_name, batch_size, seed):
                     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),  # Set the color params
                     ]
     }
-    input_shape = {'MNIST': [1, 28, 28], 'FashionMNIST': [1, 28, 28], 'cifar10': [3, 32, 32]}
+    input_shape = {'MNIST': [1, 28, 28], 'cifar10': [3, 32, 32]}
 
     dataset_class = dataset_classes[dataset_name]
     default_transform = [transforms.ToTensor(),
